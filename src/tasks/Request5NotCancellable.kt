7fe7306386c80a06cbedc9c0ae92508d1de59de4
Mike Smithson <mikesmithso@gmail.com>
2024-02-17 19:50:14 -0500

    initial commit

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -2,8 +2,22 @@ package tasks
 
 import contributors.*
 import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        GlobalScope.async(Dispatchers.Default) {
+            delay(3000)
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+
+        }
+    }
+    return deferreds.awaitAll().flatten().aggregate();
 }
--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -5,7 +5,19 @@ import contributors.*
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
-    updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) {
-    TODO()
+    updateResults: suspend (List<User>, completed: Boolean) -> Unit) {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    var allUsers = emptyList<User>()
+    repos.forEachIndexed { index, repo ->
+       val users =  service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+            allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
